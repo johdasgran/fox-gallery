75fc7c7a3415cc6e9f8b26e8b5696ee2751d9937
Johdasgran <dampig14@gmail.com>
2022-01-31 09:47:47 -0500

    add lazy load

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
+import { register_image } from "./lazy";
+
 const min = 1;
 const max = 123;
 const randomfox = () => {
-  let random = Math.floor(Math.random() * (max - min) - min);
-  random == 0 || random < 0 ? (random = 1) : "";
-  return random;
-};
+    let random = Math.floor(Math.random() * (max - min) - min);
+    random == 0 || random < 0 ? random = 1 : "" ; 
+    return random;
+} 
 
 console.log(randomfox());
 
 const container_images = document.querySelector("#images");
 
 const create_imagen = () => {
-  const container_imagen = document.createElement("div");
-  container_imagen.className = "p-4";
 
-  const imagen = document.createElement("img");
-  imagen.src = `https://randomfox.ca/images/${randomfox()}.jpg`;
-  imagen.width = "320";
-  imagen.className = "mx-auto";
+    const container_imagen = document.createElement("div");
+    container_imagen.className = "p-4 bg-gris";
 
-  container_imagen.appendChild(imagen);
+    const imagen = document.createElement("img");
+    imagen.dataset.src = `https://randomfox.ca/images/${randomfox()}.jpg`;
+    imagen.width = "320";
+    imagen.className = "mx-auto ";
+    
+    container_imagen.appendChild(imagen);
 
-  return container_imagen;
-};
+    return container_imagen;
+}
 
 const add_image = document.querySelector("#add_image");
-const action = () => container_images.append(create_imagen());
+
+const action = () => {
+    const new_imagen = create_imagen();
+    container_images.append(new_imagen);
+    register_image(new_imagen);
+}
 
 add_image.addEventListener("click", action);
---
+++ src/lazy.js
@@ -0,0 +1,24 @@
+
+const isIntersecting = (entry) => {
+    return entry.isIntersecting; // True or false if it is inside the screen
+}
+
+const load_image = (entry) => {
+    const container = entry.target;
+    const imagen = container.firstChild;
+    const url = imagen.dataset.src;
+    // load image
+    imagen.src = url;
+ 
+
+    observer.unobserve(container);
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.filter(isIntersecting).forEach(load_image)
+});
+
+export const register_image = (imagen) => {
+    // IntersectionObserver -> observer(imagen)
+    observer.observe(imagen);
+} 
