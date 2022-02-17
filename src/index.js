217a59e0e89cc5eebd005e04b58d0d29f7754428
Johdasgran <dampig14@gmail.com>
2022-02-17 18:47:54 -0500

    clean code

--- src/index.js
+++ src/index.js
@@ -3,36 +3,35 @@ import { register_image } from "./lazy.js";
 const min = 1;
 const max = 123;
 const randomfox = () => {
-    let random = Math.floor(Math.random() * (max - min) - min);
-    random == 0 || random < 0 ? random = 1 : "" ; 
-    return random;
-} 
+  let random = Math.floor(Math.random() * (max - min) - min);
+  random == 0 || random < 0 ? (random = 1) : "";
+  return random;
+};
 
 console.log(randomfox());
 
 const container_images = document.querySelector("#images");
 
 const create_imagen = () => {
+  const container_imagen = document.createElement("div");
+  container_imagen.className = "bg-gris";
 
-    const container_imagen = document.createElement("div");
-    container_imagen.className = "bg-gris";
+  const imagen = document.createElement("img");
+  imagen.dataset.src = `https://randomfox.ca/images/${randomfox()}.jpg`;
+  imagen.width = "320";
+  imagen.className = "mx-auto ";
 
-    const imagen = document.createElement("img");
-    imagen.dataset.src = `https://randomfox.ca/images/${randomfox()}.jpg`;
-    imagen.width = "320";
-    imagen.className = "mx-auto ";
-    
-    container_imagen.appendChild(imagen);
+  container_imagen.appendChild(imagen);
 
-    return container_imagen;
-}
+  return container_imagen;
+};
 
 const add_image = document.querySelector("#add_image");
 
 const action = () => {
-    const new_imagen = create_imagen();
-    container_images.append(new_imagen);
-    register_image(new_imagen);
-}
+  const new_imagen = create_imagen();
+  container_images.append(new_imagen);
+  register_image(new_imagen);
+};
 
 add_image.addEventListener("click", action);
