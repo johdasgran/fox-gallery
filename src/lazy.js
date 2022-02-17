90582979180ac9d8256a00054dd00ba44484ca14
Johdasgran <dampig14@gmail.com>
2022-02-17 18:48:13 -0500

    clean code

--- src/lazy.js
+++ src/lazy.js
@@ -1,24 +1,22 @@
-
 const isIntersecting = (entry) => {
-    return entry.isIntersecting; // True or false if it is inside the screen
-}
+  return entry.isIntersecting; // True or false if it is inside the screen
+};
 
 const load_image = (entry) => {
-    const container = entry.target;
-    const imagen = container.firstChild;
-    const url = imagen.dataset.src;
-    // load image
-    imagen.src = url;
- 
+  const container = entry.target;
+  const imagen = container.firstChild;
+  const url = imagen.dataset.src;
+  // load image
+  imagen.src = url;
 
-    observer.unobserve(container);
-}
+  observer.unobserve(container);
+};
 
 const observer = new IntersectionObserver((entries) => {
-    entries.filter(isIntersecting).forEach(load_image)
+  entries.filter(isIntersecting).forEach(load_image);
 });
 
 export const register_image = (imagen) => {
-    // IntersectionObserver -> observer(imagen)
-    observer.observe(imagen);
-} 
+  // IntersectionObserver -> observer(imagen)
+  observer.observe(imagen);
+};
